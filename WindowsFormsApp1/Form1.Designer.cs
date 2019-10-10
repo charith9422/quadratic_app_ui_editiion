a206fc4c19f6e0c1e1932326cdf03752def4b316
Charith <charith9422>
2019-10-10 17:28:38 +0530

    App added

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculate = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.a_val = new System.Windows.Forms.TextBox();
+            this.b_val = new System.Windows.Forms.TextBox();
+            this.c_val = new System.Windows.Forms.TextBox();
+            this.B = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.final_result = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(23, 107);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(75, 23);
+            this.calculate.TabIndex = 1;
+            this.calculate.Text = "Solve";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // clear
+            // 
+            this.clear.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.clear.Location = new System.Drawing.Point(23, 136);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 2;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "A";
+            // 
+            // a_val
+            // 
+            this.a_val.Location = new System.Drawing.Point(26, 56);
+            this.a_val.Name = "a_val";
+            this.a_val.Size = new System.Drawing.Size(87, 20);
+            this.a_val.TabIndex = 2;
+            this.a_val.Text = "0";
+            this.a_val.TextChanged += new System.EventHandler(this.a_val_TextChanged);
+            // 
+            // b_val
+            // 
+            this.b_val.Location = new System.Drawing.Point(148, 56);
+            this.b_val.Name = "b_val";
+            this.b_val.Size = new System.Drawing.Size(89, 20);
+            this.b_val.TabIndex = 3;
+            this.b_val.Text = "0";
+            this.b_val.TextChanged += new System.EventHandler(this.b_val_TextChanged);
+            // 
+            // c_val
+            // 
+            this.c_val.Location = new System.Drawing.Point(272, 56);
+            this.c_val.Name = "c_val";
+            this.c_val.Size = new System.Drawing.Size(78, 20);
+            this.c_val.TabIndex = 4;
+            this.c_val.Text = "0";
+            this.c_val.TextChanged += new System.EventHandler(this.c_val_TextChanged);
+            // 
+            // B
+            // 
+            this.B.AutoSize = true;
+            this.B.Location = new System.Drawing.Point(128, 59);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(14, 13);
+            this.B.TabIndex = 5;
+            this.B.Text = "B";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "C";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.B);
+            this.groupBox1.Controls.Add(this.c_val);
+            this.groupBox1.Controls.Add(this.b_val);
+            this.groupBox1.Controls.Add(this.a_val);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(356, 97);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter Coefficients:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(138, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Final Result";
+            // 
+            // final_result
+            // 
+            this.final_result.Enabled = false;
+            this.final_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.final_result.Location = new System.Drawing.Point(142, 135);
+            this.final_result.Name = "final_result";
+            this.final_result.Size = new System.Drawing.Size(208, 22);
+            this.final_result.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(111, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 33);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ax2+Bx+C";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 169);
+            this.Controls.Add(this.final_result);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Quadratic Solver";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox a_val;
+        private System.Windows.Forms.TextBox b_val;
+        private System.Windows.Forms.TextBox c_val;
+        private System.Windows.Forms.Label B;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox final_result;
+        private System.Windows.Forms.Label equation;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string a = string.Empty;
+        string b = string.Empty;
+        string c = string.Empty;
+        double input_a = 0;
+        double input_b = 0;
+        double input_c = 0;
+        double root = 0;
+        double determinent=0;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void txtInputValue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar)) e.Handled = true;         //Just Digits
+            if (e.KeyChar == (char)8) e.Handled = false;            //Allow Backspace
+        }
+
+
+        private void a_val_TextChanged(object sender, EventArgs e)
+        {
+            validate_input(a_val);
+            
+
+        }
+        private void b_val_TextChanged(object sender, EventArgs e)
+        {
+            validate_input(b_val);
+            
+        }
+        private void c_val_TextChanged(object sender, EventArgs e)
+        {
+            validate_input(c_val);
+            
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            if (a_val.Text != ""  && b_val.Text != "" && c_val.Text != "")
+            {
+                a = a_val.Text;
+                b = b_val.Text;
+                c = c_val.Text;
+                input_a = Convert.ToDouble(a);
+                input_b = Convert.ToDouble(b);
+                input_c = Convert.ToDouble(c);
+
+
+                if (input_a != 0 )
+                {
+                    determinent = Math.Pow(input_b, 2) - (4 * input_a * input_c);
+
+                    if (determinent == 0)
+                    {
+
+                        root = -input_b / (2 * input_a);
+                        double rounded_root = Math.Round(root, 3);
+                        //Console.WriteLine("Root is " + root);
+                        //MessageBox.Show("Root is " + root);
+                        final_result.Text = Convert.ToString(rounded_root);
+
+
+                    }
+                    else if (determinent > 0)
+                    {
+
+                        double root1 = ((-1 * input_b) + (Math.Pow(determinent, 0.5))) / (2 * input_a);
+                        double root2 = ((-1 * input_b) - (Math.Pow(determinent, 0.5))) / (2 * input_a);
+
+                        double rounded_root1 = Math.Round(root1, 3);
+                        double rounded_root2 = Math.Round(root2, 3);
+                        //Console.WriteLine("Roots are " + root1 + " & " + root2);
+                        //MessageBox.Show("Roots are " + root1 + " & " + root2);
+                        string r1 = Convert.ToString(rounded_root1);
+                        string r2 = Convert.ToString(rounded_root2);
+                        final_result.Text = "Roots are " + r1 + " and " + r2;
+
+                        //clear_final_result(final_result);
+                        //final_result.Text = Convert.ToString(root2);
+                    }
+                    else
+                    {
+
+                        final_result.Text = "No Real Roots!";
+
+                    }
+                }
+                else
+                {
+
+                    MessageBox.Show("Coeffcient A should not be null or zero!!!\n\tEnter again!");
+                    clear_all(a_val, b_val, c_val);
+
+                }
+
+            }
+            else {
+
+                MessageBox.Show("Coeffcient A should not be null or zero!!!\nEnter again!");
+            }
+            
+            
+                
+        }
+
+        
+
+     
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            
+            clear_all(a_val, b_val, c_val);
+            
+            
+        }
+
+        private void clear_all(TextBox a, TextBox b, TextBox c) {
+            a.Text = "0";
+            b.Text = "0";
+            c.Text = "0";
+            clear_final_result(final_result);
+        }
+
+        private void clear_final_result(TextBox a)
+        {
+            a.Text = "";
+         
+        }
+
+        private void validate_input(TextBox input) {
+
+            if (System.Text.RegularExpressions.Regex.IsMatch(input.Text, "[^-1000.0-9999.9]"))
+            {
+
+                MessageBox.Show("Please Enter Only Numbers!");
+                input.Text = string.Empty;
+            }
+            
+        }
+
+        
+    }
+}
